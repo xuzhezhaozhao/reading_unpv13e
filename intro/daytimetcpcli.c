4dde2f8a82fb0a184040b4faca2de5e635f8c555
xuzhezhao <zhezhaoxu@gmail.com>
2015-06-08 17:36:00 +0800

    update

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -15,18 +15,21 @@ main(int argc, char **argv)
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port   = htons(13);	/* daytime server */
+	servaddr.sin_port   = htons(9999);	/* daytime server */
 	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 		err_quit("inet_pton error for %s", argv[1]);
 
 	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
 		err_sys("connect error");
 
+	int cnt = 0;
 	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
 		recvline[n] = 0;	/* null terminate */
 		if (fputs(recvline, stdout) == EOF)
 			err_sys("fputs error");
+		++cnt;
 	}
+	printf("read count: %d\n", cnt);
 	if (n < 0)
 		err_sys("read error");
 
--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -13,20 +13,27 @@ main(int argc, char **argv)
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
+	/* 修改: 只 accept 127.0.0.1 的请求 */
+	if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr) <= 0) {
+		err_sys("inet_pton error");
+	}
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(13);	/* daytime server */
+	servaddr.sin_port = htons(9999);	/* daytime server */
 
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
 	Listen(listenfd, LISTENQ);
 
 	for ( ; ; ) {
+		int i;
 		connfd = Accept(listenfd, (SA *) NULL, NULL);
 
 		printf("accept one connection.\n");
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        Write(connfd, buff, strlen(buff));
+		for (i = 0; i < strlen(buff); i++) {
+        	Write(connfd, buff + i, 1);
+		}
 
 		Close(connfd);
 	}

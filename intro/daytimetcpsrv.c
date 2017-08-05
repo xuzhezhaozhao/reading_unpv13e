e593fa992ff3f42e9544b6ba5af9fb6ba5df1a03
zhezhao <zhezhaoxu@gmail.com>
2017-08-05 16:25:37 +0800

    update

--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -13,27 +13,20 @@ main(int argc, char **argv)
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
-	/* 修改: 只 accept 127.0.0.1 的请求 */
-	if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr) <= 0) {
-		err_sys("inet_pton error");
-	}
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port = htons(9999);	/* daytime server */
+	servaddr.sin_port = htons(13);	/* daytime server */
 
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
 	Listen(listenfd, LISTENQ);
 
 	for ( ; ; ) {
-		int i;
 		connfd = Accept(listenfd, (SA *) NULL, NULL);
 
 		printf("accept one connection.\n");
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-		for (i = 0; i < strlen(buff); i++) {
-        	Write(connfd, buff + i, 1);
-		}
+       	Write(connfd, buff, strlen(buff));
 
 		Close(connfd);
 	}
